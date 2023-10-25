43a6b774a7f7d9b6f3f570b999eb18697737b0ac
K eswar durgesh <142161403+EswarDurgesh@users.noreply.github.com>
2023-10-25 12:56:16 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import { Main } from './components/Main.js';
+
+export const config = {
+  'endpoint': 'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json'
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
